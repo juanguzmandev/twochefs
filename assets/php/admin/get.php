357f840e1c53e3372e5d207715bb1cdfc7ea6b23
Juan Guzman <guzmanjuan3011@gmail.com>
2021-11-16 00:35:33 -0400

    Conexión para obtener productos realizada

--- assets/php/admin/get.php
+++ assets/php/admin/get.php
@@ -5,27 +5,49 @@
 
 	$db = mysqli_connect('127.0.0.1', 'root', '', 'twochefs');
 
-	function type($type_of_request) {
+	function makeQueries($db, $query) {
 
-		switch ($type_of_request) {
+		$result = mysqli_query($db, $query);
+
+		$items = Array();
+
+		while($result_array = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+			array_push($items, $result_array);
+		}
+
+		return json_encode($items);
+
+	}
+
+	function get($type) {
+
+		switch ($type) {
 
 			case 'products':
-				
+
+				$list_products_sql = 'SELECT * FROM productos';
+
 				break;
 
 			case 'clients':
+
+				$list_clients_sql = 'SELECT * FROM clientes';
 				
 				break;
 
 			case 'invoices':
+
+				$list_invoices_sql = 'SELECT * FROM facturas';
 				
 				break;
 
-			default:
-				
-				break;
 		}
-	}
 
-	type($type_js);
+		
+
+		echo makeQueries(mysqli_connect('127.0.0.1', 'root', '', 'twochefs'), $list_products_sql);
+	
+	}
+	
+	get($type_js, $action_js);
 ?>
