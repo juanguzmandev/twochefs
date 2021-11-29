8290f61156ff9661baab4277c9f396ab07310733
Juan Guzman <guzmanjuan3011@gmail.com>
2021-11-29 17:19:27 -0400

    Registro de factura del checkout completa

--- assets/php/admin/add_invoice.php
+++ assets/php/admin/add_invoice.php
@@ -1,11 +1,24 @@
 <?php 
 
-	echo $_POST['nombre'];
-	echo $_POST['apellido'];
-	echo $_POST['telefono'];
-	echo $_POST['direccion'];
-	echo $_POST['metodo'];
-	echo $_POST['carrito'];
-	echo $_POST['total'];
+	$factura_nombre = $_POST['nombre'];
+	$factura_apellido =  $_POST['apellido'];
+	$factura_telefono = $_POST['telefono'];
+	$factura_direccion = $_POST['direccion'];
+	$factura_metodo =  $_POST['metodo'];
+	$factura_carrito = $_POST['carrito'];
+	$factura_total = $_POST['total'];
+
+	$db = mysqli_connect('127.0.0.1', 'root', '', 'twochefs');
+
+	$add_invoice_sql = 'INSERT INTO facturas(nombre, apellido, telefono, direccion, metodo, carrito, total) VALUES("'.$factura_nombre.'", "'.$factura_apellido.'", "'.$factura_telefono.'", "'.$factura_direccion.'", "'.$factura_metodo.'", \''.$factura_carrito.'\', '.$factura_total.');';
+
+	if($add_invoice_query = mysqli_query($db, $add_invoice_sql)) {
+
+		echo 'OK';
+
+	} else {
+
+		echo mysqli_error($db);
+	}
 
 ?>
