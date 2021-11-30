f928f71fcb591b16cd99a43149b8463a5439de14
Juan Guzman <guzmanjuan3011@gmail.com>
2021-11-29 21:06:24 -0400

    Descarga de factura realizada

--- assets/js/checkout/bill/getInvoiceInfo.js
+++ assets/js/checkout/bill/getInvoiceInfo.js
@@ -1,6 +1,8 @@
 
 invoiceElement = document.getElementById('invoice-holder');
 
+var invoiceNumber;
+
 const saveInvoiceInDb = invoice => {
 
 	var invoiceInfo = new FormData();
@@ -20,18 +22,32 @@ const saveInvoiceInDb = invoice => {
 
 const getInvoiceNumber = () => {
 
+	axios.post('../../assets/php/admin/get_invoice_last_register.php').then(response => {
+		invoiceNumber = response.data;
+	});	
+
 };
 
 const getInvoiceInfo = () => {
 
 	var invoice = JSON.parse(sessionStorage.getItem('invoice'));
-	//var invoiceNumber = getInvoiceNumber();
 
 	saveInvoiceInDb(invoice);
 
-	invoiceElement.innerHTML = '<h1 class="fw-bold d-flex justify-content-center">Factura Nro. 0001</h1><span class="fw-bold d-flex justify-content-center">Nombre: Juan</span><span class="d-flex justify-content-center">Apellido: Guzman</span><span class="d-flex justify-content-center">Número de teléfono: 04126945077</span><span class="d-flex justify-content-center">Dirección exacta: 04126945077</span><span class="d-flex justify-content-center">Método de pago: 04126945077</span>';
+	getInvoiceNumber();
+
+	invoiceElement.innerHTML = '<h1 class="fw-bold d-flex justify-content-center">Factura de pedido</h1><span class="fw-bold d-flex justify-content-center p-2">Nombre: '+invoice.name+'</span><span class="d-flex justify-content-center p-2">Apellido: '+invoice.lastName+'</span><span class="d-flex justify-content-center p-2">Número de teléfono: '+invoice.phone+'</span><span class="d-flex justify-content-center p-2">Dirección exacta: '+invoice.address+'</span><span class="d-flex justify-content-center p-2">Método de pago: '+invoice.method+'</span>';
 
 
 };
 
 document.addEventListener('DOMContentLoaded', getInvoiceInfo);
+
+document.getElementById('cap-button').addEventListener('click', () => {
+
+	domtoimage.toBlob(document.body)
+	.then(function(blob) {
+		saveAs(blob, 'factura.png');
+    });
+
+});
