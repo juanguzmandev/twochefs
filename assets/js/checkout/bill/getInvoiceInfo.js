4c0909cb2ae1ea39c2811b336861f4baa6e138bf
Juan Guzman <guzmanjuan3011@gmail.com>
2022-01-27 11:02:43 -0400

    Tabla de productos añadida en la factura

--- assets/js/checkout/bill/getInvoiceInfo.js
+++ assets/js/checkout/bill/getInvoiceInfo.js
@@ -1,5 +1,6 @@
 
 invoiceElement = document.getElementById('invoice-holder');
+orderElement = document.getElementById('order-holder');
 
 var invoiceNumber;
 
@@ -28,6 +29,30 @@ const getInvoiceNumber = () => {
 
 };
 
+const getOrderInfo = () => {
+
+	console.log('Hola desde getOrderInfo');
+
+	var invoice = JSON.parse(sessionStorage.getItem('invoice'));
+
+	var cart = JSON.parse(invoice.cart);
+
+	var openOrderTag = '<div class="d-flex justify-content-center mt-4"><table class="table"><thead><tr><th scope="col">Nombre</th><th scope="col"></th><th scope="col">Precio</th></tr></thead><tbody id="tableBody">';
+
+	var totalOrderTr = '<tr><th>Total</th><td></td><td class="text-success font-weight-bold">'+invoice.total+'$</td></tr>';
+
+	var closeOrderTag = '</tbody></table></div>';
+
+	cart.forEach(article => {
+		var articleTr = '<tr><td>'+article.nombre+'</td><td></td><td>'+article.precio+'$</td></tr>';
+
+		openOrderTag = openOrderTag + articleTr;
+	});
+
+	orderElement.innerHTML = openOrderTag + totalOrderTr + closeOrderTag;
+
+};
+
 const getInvoiceInfo = () => {
 
 	var invoice = JSON.parse(sessionStorage.getItem('invoice'));
@@ -40,6 +65,7 @@ const getInvoiceInfo = () => {
 
 	invoiceElement.innerHTML = '<h1 class="fw-bold d-flex justify-content-center">Factura de pedido</h1><span class="fw-bold d-flex justify-content-center p-2">Nombre: '+invoice.name+'</span><span class="d-flex justify-content-center p-2">Apellido: '+invoice.lastName+'</span><span class="d-flex justify-content-center p-2">Número de teléfono: '+invoice.phone+'</span><span class="d-flex justify-content-center p-2">Dirección exacta: '+invoice.address+'</span><span class="d-flex justify-content-center p-2">Método de pago: '+invoice.method+'</span><span class="d-flex justify-content-center p-2">Fecha de pago: '+fecha+'</span><span class="d-flex justify-content-center p-2 text-success">Total: '+invoice.total + '$' +'</span>';
 
+	getOrderInfo();
 
 };
 
--- assets/php/checkout/setOrderInDatabase.php
+++ assets/php/checkout/setOrderInDatabase.php
@@ -9,10 +9,17 @@
 	/*$nombre_captura = $_FILES['captura']['name'] = '';
 	$captura = $_FILES['captura']['tmp_name'] = '';*/
 
-	$nombre_captura = $_FILES['captura']['name'];
-	$captura = $_FILES['captura']['tmp_name'];
+	if (isset($_FILES['captura']) == true) {
 
-	$save_location = './captures/'.$nombre_captura;
+		$nombre_captura = $_FILES['captura']['name'];
+		$captura = $_FILES['captura']['tmp_name'];
+
+		$save_location = './captures/'.$nombre_captura;
+
+	} else {
+
+		$save_location = '-';
+	}
 
 	$setOrderQuery = 'INSERT INTO pedidos(nombre, apellido, telefono, direccion, fecha, pedido, total, captura) VALUES("'.$pedido['nombre'].'", "'.$pedido['apellido'].'", "'.$pedido['telefono'].'", "'.$pedido['direccion'].'","'.date("Y-m-d H:i:s").'", \''.$pedido['pedido'].'\', "'.$pedido['total'].'","'.$save_location.'")';
 
@@ -29,21 +36,33 @@
 		}
 
 	}
-	
-	if(move_uploaded_file($captura, $save_location)) {
+	if (isset($_FILES['captura']) == true) {
 
-		if(setOrderInDatabase($dbC, $pedido, $setOrderQuery)) {
+		if(move_uploaded_file($captura, $save_location)) {
 
-			echo 'OK';
+			if(setOrderInDatabase($dbC, $pedido, $setOrderQuery)) {
 
-		} else {
+				echo 'OK';
+
+			} else {
 		
-			echo 'NO';
-		}	
-	} else {
+				echo 'NO';
+			}	
+		} else {
 
 		echo "FILE NOT SAVED";
 
+		}
+	} else {
+
+		if(setOrderInDatabase($dbC, $pedido, $setOrderQuery)) {
+
+				echo 'OK';
+
+		} else {
+		
+				echo 'NO';
+		}
 	}
 
  ?>
